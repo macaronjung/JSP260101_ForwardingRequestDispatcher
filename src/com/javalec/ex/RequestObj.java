ad6ac8be3f11903a740a6e0a80527ffb2946c50b
macaronjung <macaronjung@gmail.com>
2020-02-07 00:59:35 +0900

    Initial Commit

---
+++ src/com/javalec/ex/RequestObj.java
@@ -0,0 +1,60 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestObj
+ */
+@WebServlet("/RequestObj")
+public class RequestObj extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestObj() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet");
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost");
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("actionDo");
+		
+		//컨테이너가 건내준 객체로 부터 속성 정의
+		request.setAttribute("id", "abcde");
+		request.setAttribute("pw", "12345");
+		
+		//객체 생성(위임 받을 컴포넌트의 패스를 넣어줌)
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/dispacherJsp.jsp");
+		//포워드 이용 그대로 운반(주소는 리퀘스트 서블릿파일->내용은 디스패처jsp
+		dispatcher.forward(request, response);
+		
+	}
+
+}
